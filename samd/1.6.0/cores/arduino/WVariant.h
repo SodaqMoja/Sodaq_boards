a0e32616fa52cbac76d1aa819790b0c224103d35
Gabriel Notman <g.notman@gmail.com>
2015-07-15 18:15:34 +0100

    Updated WVariant.h
    Added full list of Timer and PWM channels. Also removed TCC0 aliasing.

--- samd/1.6.0/cores/arduino/WVariant.h
+++ samd/1.6.0/cores/arduino/WVariant.h
@@ -60,20 +60,20 @@ typedef enum _ETCChannel
   TCC0_CH1 = (0<<8)|(1),
   TCC0_CH2 = (0<<8)|(2),
   TCC0_CH3 = (0<<8)|(3),
-  TCC0_CH4 = (0<<8)|(0), // Channel 4 is 0!
-  TCC0_CH5 = (0<<8)|(1), // Channel 5 is 1!
-  TCC0_CH6 = (0<<8)|(2), // Channel 6 is 2!
-  TCC0_CH7 = (0<<8)|(3), // Channel 7 is 3!
+  TCC0_CH4 = (0<<8)|(4), 
+  TCC0_CH5 = (0<<8)|(5), 
+  TCC0_CH6 = (0<<8)|(6), 
+  TCC0_CH7 = (0<<8)|(7), 
   TCC1_CH0 = (1<<8)|(0),
   TCC1_CH1 = (1<<8)|(1),
+  TCC1_CH2 = (1<<8)|(2),
+  TCC1_CH3 = (1<<8)|(3),
   TCC2_CH0 = (2<<8)|(0),
   TCC2_CH1 = (2<<8)|(1),
   TC3_CH0  = (3<<8)|(0),
   TC3_CH1  = (3<<8)|(1),
-#if defined __SAMD21J18A__
   TC4_CH0  = (4<<8)|(0),
   TC4_CH1  = (4<<8)|(1),
-#endif // __SAMD21J18A__
   TC5_CH0  = (5<<8)|(0),
   TC5_CH1  = (5<<8)|(1),
 #if defined __SAMD21J18A__
@@ -104,13 +104,19 @@ typedef enum _EPWMChannel
   PWM0_CH7=TCC0_CH7,
   PWM1_CH0=TCC1_CH0,
   PWM1_CH1=TCC1_CH1,
+  PWM1_CH2=TCC1_CH2,
+  PWM1_CH3=TCC1_CH3,
   PWM2_CH0=TCC2_CH0,
   PWM2_CH1=TCC2_CH1,
   PWM3_CH0=TC3_CH0,
   PWM3_CH1=TC3_CH1,
+  PWM4_CH0=TC4_CH0,
+  PWM4_CH1=TC4_CH1,
   PWM5_CH0=TC5_CH0,
   PWM5_CH1=TC5_CH1,
 #if defined __SAMD21J18A__
+  PWM6_CH0=TC6_CH0,
+  PWM6_CH1=TC6_CH1,
   PWM7_CH0=TC7_CH0,
   PWM7_CH1=TC7_CH1
 #endif // __SAMD21J18A__
