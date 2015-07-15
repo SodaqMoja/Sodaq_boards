da58de6113b431aae7e6fd6fc42f5206f6f8016d
Gabriel Notman <g.notman@gmail.com>
2015-07-15 17:39:05 +0100

    Added SS_2 for SD card.

--- samd/1.6.0/variants/sodaq_autonomo/variant.h
+++ samd/1.6.0/variants/sodaq_autonomo/variant.h
@@ -144,6 +144,9 @@ static const uint8_t MOSI = PIN_SPI_MOSI ;
 static const uint8_t MISO = PIN_SPI_MISO ;
 static const uint8_t SCK  = PIN_SPI_SCK ;
 
+// SD Card CS/SS pin
+static const uint8_t SS_2 = (20u)
+
 /*
  * Wire Interfaces
  */
