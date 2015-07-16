8fbf9d3064821c8501a16e5531b7a92d5ebd6daa
Gabriel Notman <g.notman@gmail.com>
2015-07-16 17:39:56 +0100

    SPI was hardcoded to use SERCOM4.
    Changed it to SERCOM3 as per our pin mapping.
    Todo: As with I2C create some system where
    this is defined in variant.h.

--- samd/1.6.0/libraries/SPI/SPI.cpp
+++ samd/1.6.0/libraries/SPI/SPI.cpp
@@ -197,4 +197,4 @@ void SPIClass::detachInterrupt() {
   // Should be disableInterrupt()
 }
 
-SPIClass SPI( &sercom4, PIN_SPI_MISO, PIN_SPI_SCK, PIN_SPI_MOSI );
+SPIClass SPI( &sercom3, PIN_SPI_MISO, PIN_SPI_SCK, PIN_SPI_MOSI );
