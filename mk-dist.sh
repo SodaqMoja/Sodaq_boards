8f2990594bbc4f8911844e3559066f74a6fddc7d
Kees Bakker <kees@m2m4all.com>
2015-08-11 21:55:16 +0200

    Add the 1.6.1 tar file to sodaqsamdboards
    This release for autonomo also uses the new 1.6 bossac

--- mk-dist.sh
+++ mk-dist.sh
@@ -3,23 +3,21 @@
 #                "checksum":"SHA-256:12868e320717f2778b5f354fe2e3ba323965e07dd22294684acda3d1bd8de0c4",
 # sed -i '/SHA-256/s/SHA-256:.*"/SHA-256:'$CRC'"/' package_sodaq_index.json
 
-VER=1.0.0
-TARFILE=sodaqboards-${VER}.tar.bz2
-tar -cjf ${TARFILE} -C avr $(cd avr && git ls-files ${VER})
-CRC=$(sha256sum ${TARFILE}  | awk '{print $1}')
-echo "\"checksum\":\"SHA-256:${CRC}\""
-echo "\"size\":\"$(cat ${TARFILE} | wc -c)\""
+doit()
+{
+    ARCH=${1?}
+    VER=${2?}
+    PREFIX=${3?}
+    TARFILE=${PREFIX}-${VER}.tar.bz2
+    tar -cjf ${TARFILE} -C ${ARCH} $(cd ${ARCH} && git ls-files ${VER})
+    CRC=$(sha256sum ${TARFILE}  | awk '{print $1}')
+    echo
+    echo "TARFILE = '${TARFILE}'"
+    echo "\"checksum\":\"SHA-256:${CRC}\""
+    echo "\"size\":\"$(cat ${TARFILE} | wc -c)\""
+}
 
-VER=1.0.1
-TARFILE=sodaqboards-${VER}.tar.bz2
-tar -cjf ${TARFILE} -C avr $(cd avr && git ls-files ${VER})
-CRC=$(sha256sum ${TARFILE}  | awk '{print $1}')
-echo "\"checksum\":\"SHA-256:${CRC}\""
-echo "\"size\":\"$(cat ${TARFILE} | wc -c)\""
-
-VER=1.6.0
-TARFILE=sodaqsamdboards-${VER}.tar.bz2
-tar -cjf ${TARFILE} -C samd $(cd samd && git ls-files ${VER})
-CRC=$(sha256sum ${TARFILE}  | awk '{print $1}')
-echo "\"checksum\":\"SHA-256:${CRC}\""
-echo "\"size\":\"$(cat ${TARFILE} | wc -c)\""
+doit avr  1.0.0 sodaqboards
+doit avr  1.0.1 sodaqboards
+doit samd 1.6.0 sodaqsamdboards
+doit samd 1.6.1 sodaqsamdboards
